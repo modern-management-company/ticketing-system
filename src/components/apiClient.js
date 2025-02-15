8aace5aff7a04bfbd39e684de7c8be461261286e
Aditya Dixt <adixit@nyu.edu>
2025-02-15 02:16:12 -0500

    add configs

--- src/components/apiClient.js
+++ src/components/apiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000',
+  baseURL: process.env.REACT_APP_API_URL || 'https://ticketing-system-6f4u.onrender.com',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json'
